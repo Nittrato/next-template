28de93e85d78a0f935288a037e05447e0b22f4c2
nitrato <carlossfloresmauel@gmail.com>
2025-10-18 19:03:28 -0300

    lito

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,28 +7,34 @@ import { perfil } from '@/lib/data';
 import Link from 'next/link';
 
 export default function Home() {
-    const { nombre, image } = perfil;
-    return (
-        <main>
-            {/* esta es la bienvenida */}
-            <section className="flex justify-between mx-4 my-8 items-center">
-                <div>
-                    <h2 className="text-h2 text-gray-400">Bienvenido {nombre} 👋</h2>
-                    <h1 className="text-h1">Listo para ver algo?</h1>
-                </div>
-                <Link href="/Perfil">
-                    <img src={image} alt="foto de perfil" className="anim animHover rounded-full h-13" />
-                </Link>
-            </section>
+	const { nombre, image } = perfil;
+	return (
+		<main>
+			{/* esta es la bienvenida */}
+			<section className="flex justify-between mx-4 my-8 items-center">
+				<div>
+					<h2 className="text-h2 text-gray-400">
+						Bienvenido {nombre} 👋
+					</h2>
+					<h1 className="text-h1">Listo para ver algo perro?</h1>
+				</div>
+				<Link href="/Perfil">
+					<img
+						src={image}
+						alt="foto de perfil"
+						className="anim animHover rounded-full h-13"
+					/>
+				</Link>
+			</section>
 
-            {/* esta es la busqueda */}
-            <Buscador />
+			{/* esta es la busqueda */}
+			<Buscador />
 
-            {/* esta son las categorias */}
-            <Categorias />
+			{/* esta son las categorias */}
+			<Categorias />
 
-            {/* esto ya es todos los productos */}
-            <Peliculas />
-        </main>
-    );
+			{/* esto ya es todos los productos */}
+			<Peliculas />
+		</main>
+	);
 }
